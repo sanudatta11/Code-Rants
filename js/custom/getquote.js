785e0c50398608ba42e9a69e9867268ca6190632
Soumyajit Dutta <sanudatta11@gmail.com>
2018-09-10 16:34:14 +0530

    Added stuff

--- js/custom/getquote.js
+++ js/custom/getquote.js
@@ -1,7 +1,8 @@
 /**
  * Created by stuxnet on 7/17/17.
  */
-
+ 
+var intervalTimer;
 var colour_n = Math.floor(Math.random() * colours.length);
 document.getElementById("promo-3").style.backgroundColor = colours[colour_n];
 
@@ -15,29 +16,48 @@ if (window.XMLHttpRequest) {
     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
 let rantText;
-console.log("R");
+var updatedText = false;
+let offlineUpdated = false;
+var n = Math.floor(Math.random() * data.data.length);
 xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        let responseObj = JSON.parse(this.responseText);
-        console.log(responseObj.rant.text.length);
-        // console.log(responseObj.rant.user_username);
-        // console.log(responseObj.rant.user_avatar.i);
-        rantText = responseObj.rant.text;
-        if (rantText.length <= 250) {
-            document.getElementById("inner_text").innerText = responseObj.rant.text;
-            document.getElementById("author").innerText = responseObj.rant.user_username;
+    if (this.readyState == 4) {
+        if (this.status >= 200 && this.status <= 304) {
+            let responseObj = JSON.parse(this.responseText);
+            console.log(responseObj.rant.text.length);
+            // console.log(responseObj.rant.user_username);
+            // console.log(responseObj.rant.user_avatar.i);
+            rantText = responseObj.rant.text;
+            if (rantText.length <= 250) {
+                document.getElementById("inner_text").innerText = responseObj.rant.text;
+                document.getElementById("author").innerText = responseObj.rant.user_username;
+                updatedText = true;
+                offlineUpdated = false;
+                console.log(intervalTimer);
+                clearInterval(intervalTimer);
+                intervalTimer = 0;
+            }
+            else {
+                xhttp.open("GET", url, true);
+                xhttp.send(null);
+            }
+        }else{
+            if(!offlineUpdated)
+            {
+                document.getElementById("inner_text").innerText = data.data[n].rant;
+                document.getElementById("author").innerText = data.data[n].author;
+            }
+            offlineUpdated = true;
+            intervalTimer = setInterval(
+                function(){
+                    xhttp.open("GET", url, true);
+                    xhttp.send(null);
+                },5000);
         }
-        else {
-            xhttp.open("GET", url, true);
-            xhttp.send(null);
-        }
-    }
-    else {
-        //Taking Static Quotes
-        var n = Math.floor(Math.random() * data.data.length);
-        // document.getElementById("inner_text").innerText = data.data[n].rant;
-        // document.getElementById("author").innerText = data.data[n].author;
     }
 };
-xhttp.open("GET", url, true);
-xhttp.send(null);
+try {
+    xhttp.open("GET", url, true);
+    xhttp.send(null);
+} catch (error) {
+    console.error(error);
+}
